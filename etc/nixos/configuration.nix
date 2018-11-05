17239a0c1647df08aea535982101fa5389d831bd
Radivarig <reslav.hollos@gmail.com>
2018-11-05 05:15:44 +0100

    Add i3 config

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -144,9 +144,76 @@
     };
 
     xsession.enable = true;
-    xsession.windowManager.i3 = {
+    xsession.windowManager.i3 = rec {
       enable = true;
       config.modifier = "Mod4";
+      config.keybindings = let
+        mod = config.modifier;
+        left = "h"; down = "j"; up = "k"; right = "l";
+        resizeSmall = "1"; resizeBig = "5";
+      in {
+        "${mod}+Return" = "exec i3-sensible-terminal";
+        "${mod}+Shift+q" = "kill";
+        "${mod}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run -i";
+
+        "${mod}+${left}"  = "focus left";
+        "${mod}+${down}"  = "focus down";
+        "${mod}+${up}"    = "focus up";
+        "${mod}+${right}" = "focus right";
+
+        "${mod}+Shift+${left}"  = "move left";
+        "${mod}+Shift+${down}"  = "move down";
+        "${mod}+Shift+${up}"    = "move up";
+        "${mod}+Shift+${right}" = "move right";
+
+        "${mod}+v" = "split toggle";
+        "${mod}+f" = "fullscreen toggle";
+
+        "${mod}+s" = "layout stacking";
+        "${mod}+w" = "layout tabbed";
+        "${mod}+e" = "layout toggle split";
+
+        "${mod}+Shift+space" = "floating toggle";
+        "${mod}+tab" = "focus mode_toggle";
+        "${mod}+minus" = "sticky toggle";
+
+        "${mod}+a" = "focus parent";
+        "${mod}+z" = "focus child";
+
+        "${mod}+1" = "workspace 1";
+        "${mod}+2" = "workspace 2";
+        "${mod}+3" = "workspace 3";
+        "${mod}+4" = "workspace 4";
+        "${mod}+5" = "workspace 5";
+        "${mod}+6" = "workspace 6";
+        "${mod}+7" = "workspace 7";
+        "${mod}+8" = "workspace 8";
+        "${mod}+9" = "workspace 9";
+
+        "${mod}+Shift+1" = "move container to workspace 1";
+        "${mod}+Shift+2" = "move container to workspace 2";
+        "${mod}+Shift+3" = "move container to workspace 3";
+        "${mod}+Shift+4" = "move container to workspace 4";
+        "${mod}+Shift+5" = "move container to workspace 5";
+        "${mod}+Shift+6" = "move container to workspace 6";
+        "${mod}+Shift+7" = "move container to workspace 7";
+        "${mod}+Shift+8" = "move container to workspace 8";
+        "${mod}+Shift+9" = "move container to workspace 9";
+
+        "${mod}+Shift+c" = "reload";
+        "${mod}+Shift+r" = "restart";
+
+        # resize (also mod + rmb)
+        "${mod}+Ctrl+${left}"  = "resize shrink width  ${resizeSmall} px or ${resizeSmall} ppt";
+        "${mod}+Ctrl+${down}"  = "resize shrink height ${resizeSmall} px or ${resizeSmall} ppt";
+        "${mod}+Ctrl+${up}"    = "resize grow   height ${resizeSmall} px or ${resizeSmall} ppt";
+        "${mod}+Ctrl+${right}" = "resize grow   width  ${resizeSmall} px or ${resizeSmall} ppt";
+
+        "${mod}+Ctrl+Shift+${left}"  = "resize shrink width  ${resizeBig} px or ${resizeBig} ppt";
+        "${mod}+Ctrl+Shift+${down}"  = "resize shrink height ${resizeBig} px or ${resizeBig} ppt";
+        "${mod}+Ctrl+Shift+${up}"    = "resize grow   height ${resizeBig} px or ${resizeBig} ppt";
+        "${mod}+Ctrl+Shift+${right}" = "resize grow   width  ${resizeBig} px or ${resizeBig} ppt";
+      };
     };
 
   };
