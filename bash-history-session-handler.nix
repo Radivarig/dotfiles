d891068dc19e99cab6b36a2e5d03bcaa67022742
Radivarig <reslav.hollos@gmail.com>
2018-12-16 16:21:36 +0100

    Simplify bash history per terminal

--- bash-history-session-handler.nix
+++ bash-history-session-handler.nix
@@ -1,51 +1,42 @@
-# Bash History Session Handler
-
+# Bash History Per Terminal
 # Updates each terminal with commands of all terminals on enter
-#     while keeping the 'per-session commands' at the end
+#   while keeping each terminals commands at the end of its history list
+# Source it from '~/.bashrc'
 
-# source it from '~/.bashrc'
 { pkgs, ... }:
-pkgs.writeScript "bash-history-session-handler" ''
-COMMON_HISTFILE="$HISTFILE"
-TTY_HISTFILE_FOLDER="$HOME"/.bhsh_tty
-\mkdir -p "$TTY_HISTFILE_FOLDER"
-TTY_HISTFILE="$TTY_HISTFILE_FOLDER"/bhsh_tty`basename $(tty)`
-SESS_HISTFILE="$TTY_HISTFILE"_session
-\cp /dev/null "$SESS_HISTFILE"
+pkgs.writeScript "bash-history-per-terminal"
+''
+COMMON_HISTFILE="$HISTFILE" # file to which to append commands
+TTY_HISTFILE="$HOME/.bhpt/tty_"`basename $(tty)`
+"install" -D /dev/null "$TTY_HISTFILE"
 
-clear_hist_list(){ \history -c;}
-set_flag_append_to_histfile(){ \history -a;}
-read_histfile_to_list(){ \history -r;}
-write_list_to_histfile(){ \history -w;}
+clear_tty_history_list(){ "history" -c;}
+flag_append_to_histfile(){ "history" -a;}
+load_histfile_to_list(){ "history" -r;}
 
-set_histfile_to_session(){ HISTFILE="$SESS_HISTFILE";}
-set_histfile_to_common(){ HISTFILE="$COMMON_HISTFILE";}
 set_histfile_to_tty(){ HISTFILE="$TTY_HISTFILE";}
+set_histfile_to_common(){ HISTFILE="$COMMON_HISTFILE";}
 
+# TODO: prevent on sigint
 prompt_cmd() {
-  set_histfile_to_session
-  set_flag_append_to_histfile
+  flag_append_to_histfile
 
-  LAST_SESS=$(tail -n 1 "$SESS_HISTFILE")
-  LAST_HIST=$(tail -n 1 "$COMMON_HISTFILE")
-
-  # add to common file, only once, skip empty
-  if [[ "$LAST_SESS" != "$LAST_HIST" && "$LAST_SESS" != "" ]]
-    then \echo "$LAST_SESS" >> "$COMMON_HISTFILE"
+  LAST_SESS_CMD=$(tail -n 1 "$TTY_HISTFILE")
+  LAST_COMMON_CMD=$(tail -n 1 "$COMMON_HISTFILE")
+  # append to common file, not multiple times, skip empty
+  if [[ "$LAST_SESS_CMD" != "$LAST_COMMON_CMD" && "$LAST_SESS_CMD" != "" ]]
+    then "echo" "$LAST_SESS_CMD" >> "$COMMON_HISTFILE"
   fi
-  clear_hist_list
 
+  clear_tty_history_list
   set_histfile_to_common
-  read_histfile_to_list
-
-  set_histfile_to_session
-  read_histfile_to_list
-
+  load_histfile_to_list
   set_histfile_to_tty
-  write_list_to_histfile
+  load_histfile_to_list
 }
 PROMPT_COMMAND="prompt_cmd;$PROMPT_COMMAND"
 
-on_exit_tty(){ \rm "$TTY_HISTFILE"{,_session};}
-\trap on_exit_tty EXIT
+# clear on exit
+on_exit_tty(){ "rm" "$TTY_HISTFILE";}
+"trap" on_exit_tty EXIT
 ''
