b1b79027111c9f71a280016c7ffbb85fb5c9299e
Radivarig <reslav.hollos@gmail.com>
2019-03-07 21:50:19 +0100

    Add ranger rc.conf and set title to current dir

--- configuration.nix
+++ configuration.nix
@@ -163,6 +163,29 @@
     };
 
     home.file.".config/lxterminal/lxterminal.conf".text = import ./lxterminal.conf.nix { inherit pkgs; };
+    home.file.".config/ranger/rc.conf".text = ''
+      map <DELETE> shell -s trash-put %s
+      set show_hidden true
+    '';
+    home.file.".config/ranger/plugins/cd_to_title.py".text = ''
+      import ranger.api
+      import os
+      import sys
+
+      old_hook_init = ranger.api.hook_init
+
+      def hook_init(fm):
+          def on_cd():
+              if fm.thisdir:
+                  title = os.path.basename(fm.thisdir.path)
+                  sys.stdout.write("\033k"+title+"\033\\")
+                  sys.stdout.flush()
+
+          fm.signal_bind('cd', on_cd)
+          return old_hook_init(fm)
+
+      ranger.api.hook_init = hook_init
+    '';
 
     home.file.".inputrc".text = ''
       $include /etc/inputrc
