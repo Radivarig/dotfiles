173b82e4a82fcab214b87c992de57b269983d2b0
Radivarig <reslav.hollos@gmail.com>
2019-03-09 16:47:07 +0100

    Add per machine settings

--- boot.nix
+++ boot.nix
@@ -1,5 +1,21 @@
-{ ... }:
-{
+{ pkgs, ... }:
+let
+machine = import ./machine-settings.nix {inherit pkgs;};
+in
+if machine.model == "xps15"
+then {
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [
+    {
+      name = "root";
+      device = "/dev/nvme0n1p6";
+      preLVM = true;
+    }
+  ];
+}
+else if machine.model == "xps13"
+then {
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda";
@@ -12,3 +28,4 @@
     }
   ];
 }
+else throw
