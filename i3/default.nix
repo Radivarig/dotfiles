535d133c05e5e6e4de06cdd64b996fe99082643e
Radivarig <reslav.hollos@gmail.com>
2019-04-11 02:19:31 +0200

    Declare i3 mod from extraConfig

--- i3/default.nix
+++ i3/default.nix
@@ -85,16 +85,17 @@ in {
       config.modifier = "Mod4";
 
       extraConfig = ''
+        set $mod ${config.modifier}
+
         for_window [class=.*] border normal 1
         for_window [class=".*"] title_format "<span>%title</span>"
         font pango:DejaVu Sans Mono 10
 
-        bindsym --release Print                     exec "${scrot}/bin/scrot -m      ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
-        bindsym --release Shift+Print               exec "${scrot}/bin/scrot -s      ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
-        bindsym --release ${config.modifier}+Print  exec "${scrot}/bin/scrot -u -d 4 ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
+        bindsym --release Print        exec "${scrot}/bin/scrot -m      ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
+        bindsym --release Shift+Print  exec "${scrot}/bin/scrot -s      ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
+        bindsym --release $mod+Print   exec "${scrot}/bin/scrot -u -d 4 ~/Screenshots/`date +%Y-%m-%d-%H-%M-%s`.png"
       '';
       config.keybindings = let
-        mod = config.modifier;
         resizeSize = "5";
         center-mouse = pkgs.writeShellScriptBin "center-mouse" ''
           sh -c 'eval `${pkgs.xdotool}/bin/xdotool getactivewindow getwindowgeometry --shell`
@@ -111,70 +112,70 @@ in {
         "XF86MonBrightnessDown" = "exec ${pkgs.xorg.xbacklight}/bin/xbacklight -dec 7";
         "XF86MonBrightnessUp"   = "exec ${pkgs.xorg.xbacklight}/bin/xbacklight -inc 3";
 
-        "${mod}+F11" = "exec compton-trans -c -7";
-        "${mod}+F12" = "exec compton-trans -c +3";
+        "$mod+F11" = "exec compton-trans -c -7";
+        "$mod+F12" = "exec compton-trans -c +3";
 
-        "${mod}+Return" = "exec ${terminal-at-title-path}/bin/terminal-at-title-path";
-        "${mod}+Shift+q" = "kill";
-        "${mod}+d" = "exec ${pkgs.dmenu}/bin/dmenu_run -i";
-        "${mod}+f" = "exec ${pkgs.i3-easyfocus}/bin/i3-easyfocus";
+        "$mod+Return" = "exec ${terminal-at-title-path}/bin/terminal-at-title-path";
+        "$mod+Shift+q" = "kill";
+        "$mod+d" = "exec ${pkgs.dmenu}/bin/dmenu_run -i";
+        "$mod+f" = "exec ${pkgs.i3-easyfocus}/bin/i3-easyfocus";
 
         # move focus/move hjkl
-        "${mod}+h" = "focus left; exec ${center-mouse}/bin/center-mouse";
-        "${mod}+j" = "focus down; exec ${center-mouse}/bin/center-mouse";
-        "${mod}+k" = "focus up; exec ${center-mouse}/bin/center-mouse";
-        "${mod}+l" = "focus right; exec ${center-mouse}/bin/center-mouse";
+        "$mod+h" = "focus left; exec ${center-mouse}/bin/center-mouse";
+        "$mod+j" = "focus down; exec ${center-mouse}/bin/center-mouse";
+        "$mod+k" = "focus up; exec ${center-mouse}/bin/center-mouse";
+        "$mod+l" = "focus right; exec ${center-mouse}/bin/center-mouse";
 
-        "${mod}+Shift+h" = "move left";
-        "${mod}+Shift+j" = "move down";
-        "${mod}+Shift+k" = "move up";
-        "${mod}+Shift+l" = "move right";
+        "$mod+Shift+h" = "move left";
+        "$mod+Shift+j" = "move down";
+        "$mod+Shift+k" = "move up";
+        "$mod+Shift+l" = "move right";
 
-        "${mod}+v" = "split v";
-        "${mod}+b" = "split h";
-        "${mod}+space" = "fullscreen toggle";
+        "$mod+v" = "split v";
+        "$mod+b" = "split h";
+        "$mod+space" = "fullscreen toggle";
 
-        "${mod}+s" = "layout stacking";
-        "${mod}+w" = "layout tabbed";
-        "${mod}+e" = "layout toggle split";
+        "$mod+s" = "layout stacking";
+        "$mod+w" = "layout tabbed";
+        "$mod+e" = "layout toggle split";
 
-        "${mod}+Tab" = "[con_mark=_last_focused] focus";
+        "$mod+Tab" = "[con_mark=_last_focused] focus";
 
-        "${mod}+Shift+space" = "floating toggle";
-        "${mod}+Shift+Tab" = "focus mode_toggle";
-        "${mod}+minus" = "sticky toggle";
+        "$mod+Shift+space" = "floating toggle";
+        "$mod+Shift+Tab" = "focus mode_toggle";
+        "$mod+minus" = "sticky toggle";
 
-        "${mod}+a" = "focus parent";
-        "${mod}+z" = "focus child";
+        "$mod+a" = "focus parent";
+        "$mod+z" = "focus child";
 
-        "${mod}+1" = "workspace 1";
-        "${mod}+2" = "workspace 2";
-        "${mod}+3" = "workspace 3";
-        "${mod}+4" = "workspace 4";
-        "${mod}+5" = "workspace 5";
-        "${mod}+6" = "workspace 6";
-        "${mod}+7" = "workspace 7";
-        "${mod}+8" = "workspace 8";
-        "${mod}+9" = "workspace 9";
+        "$mod+1" = "workspace 1";
+        "$mod+2" = "workspace 2";
+        "$mod+3" = "workspace 3";
+        "$mod+4" = "workspace 4";
+        "$mod+5" = "workspace 5";
+        "$mod+6" = "workspace 6";
+        "$mod+7" = "workspace 7";
+        "$mod+8" = "workspace 8";
+        "$mod+9" = "workspace 9";
 
-        "${mod}+Shift+1" = "move container to workspace 1";
-        "${mod}+Shift+2" = "move container to workspace 2";
-        "${mod}+Shift+3" = "move container to workspace 3";
-        "${mod}+Shift+4" = "move container to workspace 4";
-        "${mod}+Shift+5" = "move container to workspace 5";
-        "${mod}+Shift+6" = "move container to workspace 6";
-        "${mod}+Shift+7" = "move container to workspace 7";
-        "${mod}+Shift+8" = "move container to workspace 8";
-        "${mod}+Shift+9" = "move container to workspace 9";
+        "$mod+Shift+1" = "move container to workspace 1";
+        "$mod+Shift+2" = "move container to workspace 2";
+        "$mod+Shift+3" = "move container to workspace 3";
+        "$mod+Shift+4" = "move container to workspace 4";
+        "$mod+Shift+5" = "move container to workspace 5";
+        "$mod+Shift+6" = "move container to workspace 6";
+        "$mod+Shift+7" = "move container to workspace 7";
+        "$mod+Shift+8" = "move container to workspace 8";
+        "$mod+Shift+9" = "move container to workspace 9";
 
-        "${mod}+Shift+c" = "reload";
-        "${mod}+Shift+r" = "restart";
+        "$mod+Shift+c" = "reload";
+        "$mod+Shift+r" = "restart";
 
         # resize (also mod + rmb)
-        "${mod}+Ctrl+Shift+h"  = "resize shrink width  ${resizeSize} px or ${resizeSize} ppt";
-        "${mod}+Ctrl+Shift+j"  = "resize shrink height ${resizeSize} px or ${resizeSize} ppt";
-        "${mod}+Ctrl+Shift+k"  = "resize grow   height ${resizeSize} px or ${resizeSize} ppt";
-        "${mod}+Ctrl+Shift+l"  = "resize grow   width  ${resizeSize} px or ${resizeSize} ppt";
+        "$mod+Ctrl+Shift+h"  = "resize shrink width  ${resizeSize} px or ${resizeSize} ppt";
+        "$mod+Ctrl+Shift+j"  = "resize shrink height ${resizeSize} px or ${resizeSize} ppt";
+        "$mod+Ctrl+Shift+k"  = "resize grow   height ${resizeSize} px or ${resizeSize} ppt";
+        "$mod+Ctrl+Shift+l"  = "resize grow   width  ${resizeSize} px or ${resizeSize} ppt";
       };
     };
   };
