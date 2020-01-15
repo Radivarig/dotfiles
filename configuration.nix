b7251e97766a941457c481d9db05edb1ac9b1423
Radivarig <reslav.hollos@gmail.com>
2020-01-15 22:29:46 +0100

    Change feh to bg-max

--- configuration.nix
+++ configuration.nix
@@ -370,7 +370,7 @@
       while true; do ${pkgs.xkbset}/bin/xkbset m; sleep $((30)); done &
 
       while true; do
-        ${pkgs.feh}/bin/feh -z --recursive --bg-scale ~/spacebase/wallpapers;
+        ${pkgs.feh}/bin/feh -z --recursive --bg-max ~/spacebase/wallpapers;
         sleep $((5*60)); done &
     '';
   } [
